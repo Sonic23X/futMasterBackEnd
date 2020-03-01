0f9f0da8f316e356399169e5f638fcd650ee8fdd
Alfred <omar.alfredo49@gmail.com>
2020-02-29 21:30:53 -0600

    Validate 2° register

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,7 +3,7 @@
 //requires
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const moment = require('moment-timezone');
+const moment = require('moment');
 
 const User = require('../models/users');
 const configJWT = require('../configs/jwt');
@@ -45,27 +45,33 @@ let controller =
     let user = new User();
     var params = req.body;
 
-    //hash de password
-    bcrypt.hash(params.password, 5, function(err, password)
+    //buscar el correo en caso de doble registro (basico)
+    User.findOne({email: req.body.email.toLowerCase()}, function(err, user)
     {
-      if(err) return res.status(500).send({message: 'Error: Cannot save the user'});
+      if(user != null) return res.status(401).send({message: 'Email already register'});
 
-      user.name = params.name;
-      user.lastName1 = params.lastName1;
-      user.lastName2 = params.lastName2;
-      user.phone = params.phone;
-      user.email = params.email;
-      user.password = password;
-      user.photo = null;
-      user.type = 'Player';
-      user.created_at = moment().format();
-      user.active = 1;
-
-      user.save((err, userStored) => {
+      //hash de password
+      bcrypt.hash(params.password, 5, function(err, password)
+      {
         if(err) return res.status(500).send({message: 'Error: Cannot save the user'});
-        if(!userStored) return res.status(404).send({message: 'Error: Cannot found the user'});
 
-        return res.status(200).send({user: userStored});
+        user.name = params.name;
+        user.lastName1 = params.lastName1;
+        user.lastName2 = params.lastName2;
+        user.phone = params.phone;
+        user.email = params.email;
+        user.password = password;
+        user.photo = null;
+        user.type = 'Player';
+        user.created_at = moment().format();
+        user.active = 1;
+
+        user.save((err, userStored) => {
+          if(err) return res.status(500).send({message: 'Error: Cannot save the user'});
+          if(!userStored) return res.status(404).send({message: 'Error: Cannot found the user'});
+
+          return res.status(200).send({user: userStored});
+        });
       });
     });
   },
